437185916b7b3d51ee0cae5f18227a27fa633300
Shvier <daulvi@hotmail.com>
2018-11-04 21:16:16 +0800

    init commit

---
+++ VirtualCHIP-8/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  VirtualCHIP-8
+//
+//  Created by Shvier on 2018/11/4.
+//  Copyright © 2018 SuperEmulator. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
