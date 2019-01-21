b02e687fcf1121f326f29f2a7f617501e40dcdca
Shvier <daulvi@hotmail.com>
2019-01-21 17:29:00 +0800

    update some comments

--- VirtualCHIP-8/CPU.swift
+++ VirtualCHIP-8/CPU.swift
@@ -26,13 +26,13 @@ class CPU: NSObject {
     /// 0x200-0xFFF - Program ROM and work RAM
     var memory: [CChar]
     
-    /// Chip-8 has 15 8-bit general purpose registers named V0, V1 up to VE. The 16th register is used for the carry flag.
+    /// Chip-8 has 16 8-bit general purpose registers named V0, V1 up to VF. The VF register doubles as a flag for some instructions, thus it should be avoided. In addition operation VF is for carry flag.
     var registers: [CChar]
     
-    /// There is an Index register I and a program counter which can have a value from 0x000 to OxFFF
+    /// There is an 16-bit index register (For memory address)
     var registerI: CUnsignedInt
     
-    /// There is an Index register I and a program counter which can have a value from 0x000 to OxFFF
+    /// A program counter have a value from 0x000 to OxFFF
     var programCounter: CUnsignedInt
     
     /// The graphics of Chip-8 are black and white and the screen has 2048 pixels (64*32)
@@ -42,7 +42,7 @@ class CPU: NSObject {
     var delayTimer: CInt
     var soundTimer: CInt
     
-    /// when Chip-8 instruction set has opcodes that allow the program to jump to a certain address or call a subroutine, you need to implement one as part of the interpreter yourself. The system has 16 levels of stack and in order to remember which level of the stack is used, implement a stack pointer
+    /// When Chip-8 instruction set has opcodes that allow the program to jump to a certain address or call a subroutine, you need to implement one as part of the interpreter yourself. The system has 16 levels of stack and in order to remember which level of the stack is used, implement a stack pointer
     var stack: [CInt]
     var stackPoint: CInt
     
